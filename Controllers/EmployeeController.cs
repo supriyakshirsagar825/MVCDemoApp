42a8871f78333f2e255e2d60180abf6f5721b0b1
Supriya Kshirsagar <sup.kshirsagar9@gmail.com>
2021-03-07 20:34:44 -0500

    added employee Model

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var data = GetEmployeeFromController();
+            return View("~/Views/myview/Mytestview.cshtml", data);
+        }
+
+        public Employee GetEmployeeFromController()
+        {
+            return new Employee()
+            {
+                id = 1,
+                name = "supriya",
+                salary = 100000,
+                address = "1200 center g l"
+            };
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+        public string address { get; set; }
+    }
+}
