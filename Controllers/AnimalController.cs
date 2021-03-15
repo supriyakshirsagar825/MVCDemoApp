d9d0fff085cf4bca3b25056ee959baa486317a66
Supriya Kshirsagar <https://github.com/supriyakshirsagar825>
2021-03-15 15:17:39 -0400

    Render Section in Mvc

---
+++ Controllers/AnimalController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: Animal
+        public ActionResult Index()
+        {
+            return View("Index", "_LayoutWithSection");
+        }
+        [HttpPost]
+        public ActionResult Index1(string txtName)
+        {
+            ViewBag.Name = txtName;
+            return View("Index", "_LayoutWithSection");
+        }
+    }
+}
