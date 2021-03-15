b9c05701fe9f210be074d15dad64e5456ae0824c
Supriya Kshirsagar <https://github.com/supriyakshirsagar825>
2021-03-15 19:45:35 -0400

    test

---
+++ Controllers/Test2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class Test2Controller : Controller
+    {
+        // GET: Test2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
